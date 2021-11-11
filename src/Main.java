7f070a9fe8be4812bf32d559a022c0ae9cfcc750
Сыдыкова Раушан <raushan.s@sa.ncn.kz>
2021-11-11 13:34:04 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentsList studentsList = new StudentsList();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите информацию о студенте: \"ФИО, номер группы, номер студенческого билета\"");
+
+        while (true) {
+            String[] segments = scanner.nextLine().split(",");
+            if (segments[0].equals("end")) {
+                break;
+            }
+            Student student = new Student(segments[0].trim(), segments[0].trim(), segments[0].trim());
+            studentsList.addStudent(student);
+
+            System.out.println("Введите информация о студенте (для завершения работы программы введите \"end\")");
+        }
+        studentsList.printList();
+    }
+}
+
